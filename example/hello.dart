200c42053b3a7c7e2344ece5059db8ce09bec178
Your Name <royanhusen77@gmail.com>
2022-09-29 14:02:45 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,11 @@
+import 'package:dart_packages_library/hello.dart';
+import 'package:dart_packages_library/src/customer.dart';
+
+void main() {
+  print(sayHello("Royyan"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+}
---
+++ lib/hello.dart
@@ -0,0 +1,10 @@
+
+library dart_packages_library;
+
+
+/// semua di export
+// export 'src/say_hello.dart';
+
+/// jika tidak semua ingin di export
+export 'src/say_hello.dart' show sayHello;
+
